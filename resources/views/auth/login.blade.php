1470a62fadde5de57606f8147cd1d9f443774b31
Auxiliar programacion Instituto de Fisica <apoyoprogramacionif@udea.edu.co>
2018-09-18 13:32:29 -0500

    Creacion vistas login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Main CSS-->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!-- Font-icon css-->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Login - {{ config('app.name')}}</title>
+  </head>
+  <body>
+    <section class="material-half-bg">
+      <div class="cover"></div>
+    </section>
+    <section class="login-content">
+      <div class="logo">
+        <h1>{{ config('app.name') }}</h1>
+      </div>
+      <div class="login-box">
+        <form method="POST" action="{{ route('login') }}" class="login-form">
+            @csrf
+          <h3 class="login-head"><i class="fa fa-lg fa-fw fa-user"></i>Ingreso al sistema</h3>
+            <div class="form-group">
+                <label for="email" class="control-label">Email</label>
+                <input
+                id="email"
+                type="email"
+                class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                name="email"
+                value="{{ old('email') }}"
+                placeholder="email"
+                required
+                autofocus>
+                @if ($errors->has('email'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <label for="contraseña" class="control-label">{{ __('Contraseña') }}</label>
+                <input
+                id="contraseña"
+                type="password"
+                class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                name="contraseña"
+                placeholder="contraseña"
+                required>
+                @if ($errors->has('password'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <p class="semibold-text mb-2">
+                    <a href="" data-toggle="flip">
+                    <!-- <a class="btn btn-link" href=""> -->
+                        Recuperar contraseña
+                    </a>
+                </p>
+                <p class="semibold-text mb-2">
+                    <a href="" data-toggle="flip">
+                    <!-- <a class="btn btn-link" href=""> -->
+                        Recuperar usuario
+                    </a>
+                </p>
+            </div>
+            <div class="form-group btn-container">
+                <button type="submit" class="btn btn-primary btn-block">
+                    {{ __('Ingresar') }}
+                </button>
+            </div>
+        </form>
+      </div>
+    </section>
+    <!-- Essential javascripts for application to work-->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+    <!-- The javascript plugin to display page loading on top-->
+    <script src="js/plugins/pace.min.js"></script>
+    <script type="text/javascript">
+      // Login Page Flipbox control
+      $('.login-content [data-toggle="flip"]').click(function() {
+      	$('.login-box').toggleClass('flipped');
+      	return false;
+      });
+    </script>
+  </body>
+</html>
+
+
+
+
+
+
+
+
+
+
